73e95ef688c26abf591d7ec93dd6abdcea45918d
Dave <davemers@iu.edu>
2020-01-27 18:55:52 -0500

    more fixes to rand color gen2

--- matlab/generate_rand_color_image2.m
+++ matlab/generate_rand_color_image2.m
@@ -12,8 +12,39 @@ plot_num = 1;
 %% shape parameters
 
 %color = {'blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white', [0.5, 0.5, 0.5]};
-color = {[0 0 1]; [0 1 0]; [1 0 0]; [0 1 1]; [1 0 1]; [1 1 0]; [0 0 0]; [1 1 1]; [0.5 0.5 0.5]};                    
+% color = {[0 0 1]; [0 1 0]; [1 0 0]; [0 1 1]; [1 0 1]; [1 1 0]; [0 0 0]; [1 1 1]; [0.5 0.5 0.5]};                    
 
+% http://alumni.media.mit.edu/~wad/color/numbers.html
+color = {[0, 0, 0];[87, 87, 87]/255;[173, 35, 35]/255;[42, 75, 215]/255;...
+         [29, 105, 20]/255;[129, 74, 25]/255;[129, 38, 192]/255;[160, 160, 160]/255;...
+         [129, 197, 122]/255;[157, 175, 255]/255;[41, 208, 208]/255;[255, 146, 51]/255;...
+         [255, 238, 51]/255;[233, 222, 187]/255;[255, 205, 243]/255;[255, 255, 255]/255};
+
+commandwindow;
+
+%% create the folders
+save_path = 'D:/IUPUI/Test_data/test_blur4/';
+
+warning('off');
+mkdir(save_path);
+mkdir(save_path, 'images');
+mkdir(save_path, 'depth_maps');
+warning('on');
+
+%% show the colors
+img_w = 50;
+img_h = 250;
+img = [];
+
+for idx=1:numel(color)
+    img = cat(2, img, cat(3, color{idx}(1).*ones(img_h, img_w), color{idx}(2).*ones(img_h, img_w), color{idx}(3).*ones(img_h, img_w))); 
+end
+
+figure(plot_num)
+imshow(img)
+plot_num = plot_num + 1;
+
+%% start to create the images
 img_w = 400;
 img_h = 400;
 
@@ -21,41 +52,44 @@ img_h = 400;
 circle = [1,img_w; 1,img_h; 10,20];
 polygon = [1,img_w; 1,img_h; -50,50];
 
-commandwindow;
-
-%% start to create the images
-dm_values = [0, 8:1:232];
-
-save_path = 'D:/IUPUI/Test_data/test_blur3/';
+dm_values = [0, 9:1:232];
 
 fprintf('%s\n\n', save_path);
 
-for kdx=0:255
+tic;
+for kdx=0:49
 
-    C = randi([1,numel(color)],1);
-    img = cat(3, color{C}(1).*ones(img_w, img_h), color{C}(2).*ones(img_w, img_h), color{C}(3).*ones(img_w, img_h));
-%     img = zeros(img_w, img_h, 3);
-    dm = zeros(img_w, img_h, 3);
+    % get the random background color
+    bg_color = randi([1,numel(color)],1);
+    img = cat(3, color{bg_color}(1).*ones(img_h, img_w), color{bg_color}(2).*ones(img_h, img_w), color{bg_color}(3).*ones(img_h, img_w));
+    dm = zeros(img_h, img_w, 3);
+    
+    D = randi([1, numel(dm_values)],1,50);
+    D = sort(unique(D));
 
-    for idx=1:numel(dm_values)
+    for idx=1:numel(D)
 
         % get the number of shapes for a given depth map value
-        N = randi([0,5], 1);
+%         N = randi([3,floor((numel(D)-idx)/2) + 6], 1);
+        N = randi([3,floor(exp((4*(numel(D)-idx))/numel(D))) + 6], 1);
 
-        dm_val = [idx, idx, idx]/255.0;
+        dm_val = (dm_values(D(idx))/255)*ones(1,3); %, dm_values((idx, idx]/255.0;
         
         for jdx=1:N
             % get the shape type
             T = randi([1,2], 1);
-%             V = idx;
-
+            
+            % get the random color for the shape
+            C = randi([1,numel(color)],1);
+            while(C == bg_color)
+                C = randi([1,numel(color)],1);
+            end
+            
             switch(T)
-
                 case 1
                     X = randi(circle(1,:), 1);
                     Y = randi(circle(2,:), 1);
                     R = randi(circle(3,:), 1);
-                    C = randi([1,numel(color)],1);
 
                     img = insertShape(img, 'FilledCircle', [X, Y, R], 'Color', color{C}, 'Opacity',1, 'SmoothEdges', false);
                     dm = insertShape(dm, 'FilledCircle', [X, Y, R], 'Color', dm_val, 'Opacity',1, 'SmoothEdges', false);
@@ -68,8 +102,6 @@ for kdx=0:255
                     P(2:2:end) = P(2:2:end) + Y;
                     P = cat(2, X, Y, P);
 
-                    C = randi([1,numel(color)],1);
-
                     img = insertShape(img, 'FilledPolygon', P, 'Color', color{C}, 'Opacity',1, 'SmoothEdges', false);
                     dm = insertShape(dm, 'FilledPolygon', P, 'Color', dm_val, 'Opacity',1, 'SmoothEdges', false);
 
@@ -77,10 +109,10 @@ for kdx=0:255
         end
     end
 
-%     figure(1)
+%     figure(plot_num)
 %     imshow(img);
 % 
-%     figure(2)
+%     figure(plot_num + 1)
 %     imshow(dm);
 
     % save the image file and depth maps
@@ -92,33 +124,33 @@ for kdx=0:255
     dm_filename = strcat('depth_maps/dm_', image_num, '.png');
     imwrite(dm, strcat(save_path, dm_filename));
     
-    fprintf('%s, %s, 0.32, 0.01, 256, \n', img_filename, dm_filename);
+    fprintf('%s, %s, 0.32, 0.01, 256\n', img_filename, dm_filename);
     
 end
 
-return;
+toc;
 
 %% this adds a capabilityy to write out individual depth maps
 
-min_depth = 0;
-max_depth = 255;
-
-for idx=min_depth:max_depth
-    gt = idx*ones(300,300);
-
-    imwrite(uint8(gt), strcat('D:/IUPUI/Test_data/test_blur/depth_',num2str(idx,'%03d'),'.png'));
-
-end
+% min_depth = 0;
+% max_depth = 255;
+% 
+% for idx=min_depth:max_depth
+%     gt = idx*ones(300,300);
+% 
+%     imwrite(uint8(gt), strcat('D:/IUPUI/Test_data/test_blur/depth_',num2str(idx,'%03d'),'.png'));
+% 
+% end
 
 %% write out the blur image input
 
-for idx=0:4
-
-    for jdx=0:255
-        fprintf('images/image_%02d.png, depth_maps/depth_%03d.png, 0.32, 0.01, 256, %03d\n', idx, jdx, jdx);
-    end
-
-end
+% for idx=0:4
+% 
+%     for jdx=0:255
+%         fprintf('images/image_%02d.png, depth_maps/depth_%03d.png, 0.32, 0.01, 256, %03d\n', idx, jdx, jdx);
+%     end
+% 
+% end
 
 
 
